85598bf44df1b140f1b01d6706d62ec3fc1bba39
Pedro Ivo <p_ivonogueira@hotmail.com>
2023-02-02 21:21:35 -0300

    Implementação Delete

--- JDBC/dao-jdbc/src/application/Program.java
+++ JDBC/dao-jdbc/src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,6 +13,7 @@ public class Program {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("=== TESTE 1: seller findById ===");
@@ -43,6 +45,13 @@ public class Program {
 		seller.setName("Martha Wayne");
 		sellerDao.update(seller);
 		System.out.println("Update completed");
+		
+		System.out.println("\n=== TEST 6: seller delete =====");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed");
+		sc.close();
 		
 	}
 
--- JDBC/dao-jdbc/src/model/dao/impl/SellerDaoJDBC.java
+++ JDBC/dao-jdbc/src/model/dao/impl/SellerDaoJDBC.java
@@ -53,14 +53,12 @@ public class SellerDaoJDBC implements SellerDao {
 				else {
 					throw new DbException("Unexpected error! No rows affected!");
 				}
-			
 		}
 		catch (SQLException e) {
 			throw new DbException(e.getMessage());
 		}
 		finally {
 			DB.closeStatement(st);
-			
 		}
 	}
 
@@ -89,16 +87,26 @@ public class SellerDaoJDBC implements SellerDao {
 			DB.closeStatement(st);
 			
 		}
-
-
-
 	}
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
-
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("DELETE FROM seller "
+					+ "WHERE Id = ?");
+					
+					st.setInt(1, id);
+					st.executeUpdate();
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 	}
+
 
 	@Override
 	public Seller findById(Integer id) {
