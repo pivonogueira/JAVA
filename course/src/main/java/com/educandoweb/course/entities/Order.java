4bb31c7ba7d66e378d3ba537002694f2054356dd
Pedro Ivo <p_ivonogueira@hotmail.com>
2023-07-12 20:32:12 -0300

    Add produc-order association

--- course/src/main/java/com/educandoweb/course/entities/Order.java
+++ course/src/main/java/com/educandoweb/course/entities/Order.java
@@ -31,6 +31,8 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
+
+
     public Order() {
     }
 
--- course/src/main/java/com/educandoweb/course/entities/Product.java
+++ course/src/main/java/com/educandoweb/course/entities/Product.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -23,6 +24,9 @@ public class Product implements Serializable {
     @ManyToMany
     @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
 
     public Product() {
@@ -78,6 +82,15 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+        for (OrderItem x : items){
+            set.add(x.getOrder());
+        }
+        return set;
     }
 
     @Override
