6b74197fadf1dab592e501761b2000fdbb5c4731
Pedro Ivo <p_ivonogueira@hotmail.com>
2023-07-13 20:10:51 -0300

    Add payment association

--- course/src/main/java/com/educandoweb/course/config/TestConfig.java
+++ course/src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -70,6 +70,10 @@ public class TestConfig implements CommandLineRunner {
 
         orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 
+        Payment pay1 = new Payment(null, Instant.parse("2023-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
     }
 
 }
--- course/src/main/java/com/educandoweb/course/entities/Order.java
+++ course/src/main/java/com/educandoweb/course/entities/Order.java
@@ -31,7 +31,8 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
-
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
 
     public Order() {
     }
@@ -75,6 +76,14 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     public Set<OrderItem> getItems() {
