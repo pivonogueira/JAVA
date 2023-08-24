035daaa00315fedbefca6c9fe881d95d587d6708
Pedro Ivo <p_ivonogueira@hotmail.com>
2023-08-23 21:40:29 -0300

    Add User Insert

--- course/src/main/java/com/educandoweb/course/services/UserService.java
+++ course/src/main/java/com/educandoweb/course/services/UserService.java
@@ -22,4 +22,8 @@ public class UserService {
         Optional<User> obj = repository.findById(id);
         return obj.get();
     }
+
+    public User insert(User user){
+        return repository.save(user);
+    }
 }
